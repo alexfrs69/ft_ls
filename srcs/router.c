be33e2f1d0d686ca2359776245b985ce6a192951
Alexandre FRANCOIS <afrancoi@student.42.fr>
2019-05-19 07:47:56 +0200

    variable name char missing

--- srcs/router.c
+++ srcs/router.c
@@ -6,16 +6,16 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 15:29:54 by afrancoi          #+#    #+#             */
-/*   Updated: 2019/05/06 13:15:00 by afrancoi         ###   ########.fr       */
+/*   Updated: 2019/05/19 07:10:43 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	route_to(t_queue *queu, int listoptions)
+void	route_to(t_queue *queue, int listoptions)
 {
 	if ((listoptions & OPT_R))
-		options_recursive(queu, listoptions);
+		options_recursive(queue, listoptions);
 	else
-		queue_file(queu, listoptions);
+		queue_file(queue, listoptions);
 }
