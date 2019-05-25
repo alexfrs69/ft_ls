c1555fcb6e9eb0e7a6342eb4b0003c57f98b90f2
Alexandre FRANCOIS <afrancoi@student.42.fr>
2019-05-25 11:57:10 +0200

    error fix

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 03:14:23 by afrancoi          #+#    #+#             */
-/*   Updated: 2019/05/25 09:23:31 by afrancoi         ###   ########.fr       */
+/*   Updated: 2019/05/25 11:00:58 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ void	ft_error(int ernb, char *path)
 	char *tmp;
 
 	if((tmp = ft_strrchr(path, '/')))
-		path = tmp + 1;
+		tmp++;
+	else
+		tmp = path;
 	ft_putstr_fd(path, 2);
 	ft_putendl_fd(":", 2);
 	ft_putstr_fd("ft_ls: ", 2);
-	ft_putstr_fd(path, 2);
+	ft_putstr_fd(tmp, 2);
 	ft_putstr_fd(": ", 2);
 	ft_putendl_fd(strerror(ernb), 2);
 }
