6210016ebb5e92664f50db50ff3e68347d80d99c
Alexandre FRANCOIS <afrancoi@student.42.fr>
2019-05-25 03:49:08 +0200

    error displayed on stderr

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 03:14:23 by afrancoi          #+#    #+#             */
-/*   Updated: 2019/05/25 03:35:39 by afrancoi         ###   ########.fr       */
+/*   Updated: 2019/05/25 03:48:56 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 
 void	ft_error(int ernb, char *path)
 {
-	ft_putstr(path);
-	ft_putendl(":");
-	ft_putstr("ft_ls: ");
+	ft_putstr_fd(path, 2);
+	ft_putendl_fd(":", 2);
+	ft_putstr_fd("ft_ls: ", 2);
 	path = ft_strrchr(path, '/');
-	ft_putstr(path + 1);
-	ft_putstr(": ");
-	ft_putendl(strerror(ernb));
-	ft_putchar('\n');
+	ft_putstr_fd(path + 1, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putendl_fd(strerror(ernb), 2);
+	ft_putchar_fd('\n', 2);
 }
