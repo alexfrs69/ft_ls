84ae28ed9134d4d87d95a584d8d8398d439da9f7
afrancoi <afrancoi@student.42.fr>
2019-05-31 11:55:52 +0200

    fix sorting by nanosec

--- srcs/sort_utils.c
+++ srcs/sort_utils.c
@@ -3,23 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   sort_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 06:43:47 by afrancoi          #+#    #+#             */
-/*   Updated: 2019/05/30 12:48:21 by root             ###   ########.fr       */
+/*   Updated: 2019/05/31 11:54:05 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 #include "libft.h"
-#include <time.h>
 
 int		check_time(t_file *a, t_file *b)
 {
-	if (a->stat.st_mtime >= b->stat.st_mtime)
-		return (1);
+	long int	asec;
+	long int	bsec;
+	int			ret;
+
+	asec = a->stat.st_mtim.tv_sec;
+	bsec = b->stat.st_mtim.tv_sec;
+	if (asec > bsec)
+		ret = 1;
+	else if (asec == bsec)
+	{
+		if (a->stat.st_mtim.tv_nsec > b->stat.st_mtim.tv_nsec)
+			ret = 1;
+		else
+			ret = 0;
+
+	}
 	else
-		return (0);
+		ret = 0;
+	return(ret);
 }
 
 int		check_strcmp(t_file *a, t_file *b)
