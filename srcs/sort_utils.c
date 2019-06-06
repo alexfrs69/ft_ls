9a2e16b44548e6a938c655170fb7209b3b794494
afrancoi <afrancoi@student.42.fr>
2019-06-06 23:28:56 +0200

    reverse sorting

--- srcs/sort_utils.c
+++ srcs/sort_utils.c
@@ -6,7 +6,7 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 06:43:47 by afrancoi          #+#    #+#             */
-/*   Updated: 2019/06/06 20:12:38 by afrancoi         ###   ########.fr       */
+/*   Updated: 2019/06/06 22:40:03 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,28 @@ int		check_time(t_file *a, t_file *b)
 	return(ret);
 }
 
+int		reverse_time(t_file *a, t_file *b)
+{
+	long int	asec;
+	long int	bsec;
+	int			ret;
+
+	asec = a->stat.st_mtim.tv_sec;
+	bsec = b->stat.st_mtim.tv_sec;
+	if (asec < bsec)
+		ret = 1;
+	else if (asec == bsec)
+	{
+		if (a->stat.st_mtim.tv_nsec <= b->stat.st_mtim.tv_nsec)
+			ret = 1;
+		else
+			ret = 0;
+	}
+	else
+		ret = 0;
+	return(ret);
+}
+
 int		check_strcmp(t_file *a, t_file *b)
 {
 	return (ft_strcasecmp(a->name, b->name) <= 0);
@@ -48,7 +70,12 @@ int		reverse_cmp(t_file *a, t_file *b)
 t_cmp ft_getcmp(int opts)
 {
 	if (opts & OPT_T)
-		return (&check_time);
+	{
+		if (opts & OPT_REV)
+			return (&reverse_time);
+		else
+			return (&check_time);
+	}
 	else if (opts & OPT_REV)
 		return (&reverse_cmp);
 	else
